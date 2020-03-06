505072f5d4e598bbfd74318d8d96f8260c5d1f2d
Thomas W <tomwim@users.noreply.github.com>
2020-03-06 11:46:21 +0100

    Add Scene Setup

---
+++ Source/Utils/SceneSetup/OnInteractionFrameworkSetup.cs
@@ -0,0 +1,14 @@
+﻿using Innoactive.Creator.Utils;
+
+namespace Innoactive.Creator.Interaction.Utils
+{
+    /// <summary>
+    /// This base class is supposed to be implemented by classes which will be called to setup the scene,
+    /// specifically interaction frameworks.
+    /// </summary>
+    public abstract class OnInteractionFrameworkSetup : OnSceneSetup
+    {
+        /// <inheritdoc />
+        public override string Key { get; } = "InteractionFrameworkSetup";
+    }
+}
