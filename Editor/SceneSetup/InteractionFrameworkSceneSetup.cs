2cae45945bf2806bb712cf7ea440dbb2d89967ad
Gustavo Quiroz <gusinuhe@gmail.com>
2020-03-26 19:39:57 +0100

    chg: Rename InteractionFrameworkSetup to InteractionFrameworkSceneSetup

--- Editor/SceneSetup/InteractionFrameworkSceneSetup.cs
+++ Editor/SceneSetup/InteractionFrameworkSceneSetup.cs
@@ -4,7 +4,7 @@
     /// This base class is supposed to be implemented by classes which will be called to setup the scene,
     /// specifically interaction frameworks.
     /// </summary>
-    public abstract class OnInteractionFrameworkSetup : OnSceneSetup
+    public abstract class InteractionFrameworkSceneSetup : SceneSetup
     {
         /// <inheritdoc />
         public override int Priority { get; } = 10;
