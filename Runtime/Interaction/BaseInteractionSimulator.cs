4637c85e8ca8195b28c657382a6e9af0c0dcdad2
Simon L <simon.lerch@innoactive.de>
2021-03-30 15:22:45 +0200

    refactor: adding new snapzone and interactable base object

---
+++ Runtime/Interaction/BaseInteractionSimulator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using Innoactive.Creator.BasicInteraction;
+using Innoactive.Creator.Core.Utils;
+using UnityEngine.SceneManagement;
+
+namespace Innoactive.Creator.BasicInteraction
+{
+    /// <summary>
+    /// Base interaction simulator, only have one concrete simulator implementation in your project.
+    /// If no concrete implementation is found a <see cref="BaseInteractionSimulatorDummy"/> will be used.
+    /// </summary>
+    public abstract class BaseInteractionSimulator
+    {
+        private static BaseInteractionSimulator instance;
+        
+        /// <summary>
+        /// Current instance of the interaction simulator.
+        /// </summary>
+        public static BaseInteractionSimulator Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    Type type = ReflectionUtils
+                        .GetConcreteImplementationsOf(typeof(BaseInteractionSimulator))
+                        .FirstOrDefault(t => t != typeof(BaseInteractionSimulatorDummy));
+
+                    if (type == null)
+                    {
+                        type = typeof(BaseInteractionSimulatorDummy);
+                    }
+
+                    instance = (BaseInteractionSimulator)ReflectionUtils.CreateInstanceOfType(type);
+                    SceneManager.sceneUnloaded += OnSceneLoad;
+                }
+
+                return instance;
+            }
+        }
+
+        private static void OnSceneLoad(Scene scene)
+        {
+            instance = null;
+            SceneManager.sceneUnloaded -= OnSceneLoad;
+        }
+
+        /// <summary>
+        /// Simulates touching the given object. Expected behavior is that the object stays touched until StopTouch is called.
+        /// </summary>
+        public abstract void Touch(IInteractableObject interactable);
+
+        /// <summary>
+        /// Simulates stop touching the given object.
+        /// </summary>
+        public abstract void StopTouch();
+
+        /// <summary>
+        /// Simulates grabbing the given object.
+        /// </summary>
+        public abstract void Grab(IInteractableObject interactable);
+
+        /// <summary>
+        /// Simulates releasing the given object.
+        /// </summary>
+        public abstract void Release();
+
+        /// <summary>
+        /// Simulates usage of the object and keeps using the given object until StopUse is called.
+        /// </summary>
+        public abstract void Use(IInteractableObject interactable);
+
+        /// <summary>
+        /// Simulates stop using the given object.
+        /// </summary>
+        public abstract void StopUse(IInteractableObject interactable);
+    }
+}
---
+++ Runtime/Interaction/InteractionSimulatorDummy.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Innoactive.Creator.BasicInteraction
+{
+    /// <summary>
+    /// Interaction simulator dummy, does nothing beside of warning to there is no concrete implementation of a simulator.
+    /// </summary>
+    public class BaseInteractionSimulatorDummy : BaseInteractionSimulator
+    {
+        private const string ErrorMessage = "You are using the interaction simulator without providing a concrete implementation for your VR interaction framework.";
+        
+        /// <inheritdoc />
+        public override void Touch(IInteractableObject interactable)
+        {
+            Debug.LogWarning(ErrorMessage);
+        }
+
+        /// <inheritdoc />
+        public override void StopTouch()
+        {
+            Debug.LogWarning(ErrorMessage);
+        }
+
+        /// <inheritdoc />
+        public override void Grab(IInteractableObject interactable)
+        {
+            Debug.LogWarning(ErrorMessage);
+        }
+
+        /// <inheritdoc />
+        public override void Release()
+        {
+            Debug.LogWarning(ErrorMessage);
+        }
+
+        /// <inheritdoc />
+        public override void Use(IInteractableObject interactable)
+        {
+            Debug.LogWarning(ErrorMessage);
+        }
+
+        /// <inheritdoc />
+        public override void StopUse(IInteractableObject interactable)
+        {
+            Debug.LogWarning(ErrorMessage);
+        }
+    }
+}
